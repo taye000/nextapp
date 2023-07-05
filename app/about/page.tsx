ef3b724d1acfc09e194471d1174997392e3227e9
taye000 <taylorgitari@gmail.com>
2023-07-05 12:31:58 +0200

    about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 
-export default function Home() {
+const about = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1 className={`mb-3 text-4xl font-bold`}>Imani Escrow Services</h1>
+        <h1 className={`mb-3 text-4xl font-bold`}>About Us</h1>
         <Link
           href={"/"}
           className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
@@ -32,18 +31,13 @@ export default function Home() {
           Signup
         </Link>
       </div>
-
-      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div> */}
-
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className={`m-0 max-w-[60ch] text-sm opacity-50`}>
+          We are a company aiming to provide a safe platform for small
+          enterprises to secure their e-commerce businesses by securing the
+          payment process, through offering an escrow service.
+        </p>
+      </div>
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <Link
           href={"/faq"}
@@ -77,4 +71,6 @@ export default function Home() {
       </div>
     </main>
   );
-}
+};
+
+export default about;
