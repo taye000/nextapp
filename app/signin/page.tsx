406a021b1f1f58d6bd16248869b660298c0864e4
taye000 <taylorgitari@gmail.com>
2023-07-05 15:40:58 +0200

    more info

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -6,9 +6,11 @@ const signin = () => {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1 className={`mb-3 text-4xl font-bold`}>Sign in</h1>
-        
       </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <p className={`m-0 max-w-[30ch] text-sm`}>
+        Don't have an account,
+          </p>
         <Link
           href={"/signup"}
           className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -5,9 +5,17 @@ const signup = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1 className={`mb-3 text-4xl font-bold`}>Register</h1>
+        <h1 className={`mb-3 text-4xl font-bold`}>
+          Welcome to Imani Escrow Services
+        </h1>
+        <p className={`m-0 max-w-[30ch] text-sm`}>
+        Create your account
+          </p>
       </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <p className={`m-0 max-w-[30ch] text-sm`}>
+        Already have an account,
+          </p>
         <Link
           href={"/signin"}
           className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
