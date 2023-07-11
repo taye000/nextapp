fa0d01eb45b23f4ee39e65eb09ea8f73171fd6e9
taye000 <taylorgitari@gmail.com>
2023-07-11 19:16:29 +0200

    headers

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -4,8 +4,10 @@ import React from "react";
 const faq = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+      <header>FAQ's</header>
+      </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      <h1 className={`mb-3 text-4xl font-bold`}>FAQ's</h1>
         <Link
           href={"/"}
           className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
