71a0ffb86ce9f2fe27f0cde62593a901db761258
taye000 <taylorgitari@gmail.com>
2023-07-14 20:30:51 +0200

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <footer id="main-footer">
+        <p>Copyright &copy; 2023 Imani Escrow Services</p>
+        </footer></body>
     </html>
   )
 }
