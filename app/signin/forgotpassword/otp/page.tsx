d24711c756ca695009171842fd0354acf1ce8338
taye000 <taylorgitari@gmail.com>
2023-07-14 12:09:52 +0200

    buttons divs

--- app/signin/forgotpassword/otp/page.tsx
+++ app/signin/forgotpassword/otp/page.tsx
@@ -5,36 +5,35 @@ const otp = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <header>Enter Recovery Code</header>
-      </div>
-      <div>
-        <p className={`m-50 max-w-[30ch] text-4xl`}>One-Time-Pass</p>
-        <form className="flex flex-col space-y-4">
-          <input
-            type="Code"
-            name="Code"
-            id="Code"
-            className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-          />
-          <button
-            type="submit"
-            className="button"
-          >
-            Continue
-          </button>
-          <Link
-            href={"/signup"}
-            className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-          >
-            Sign up
-          </Link>
-          <Link
-            href={"/signin"}
-            className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-          >
-            sign in
-          </Link>
-        </form>
+        <div>
+          <header>Enter Recovery Code</header>
+        </div>
+        <div>
+          <p className={`m-50 max-w-[30ch] text-4xl`}>One-Time-Pass</p>
+          <form className="flex flex-col space-y-4">
+            <input
+              type="Code"
+              name="Code"
+              id="Code"
+              className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+            />
+            <button type="submit" className="button">
+              Continue
+            </button>
+            <Link
+              href={"/signup"}
+              className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+            >
+              Sign up
+            </Link>
+            <Link
+              href={"/signin"}
+              className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+            >
+              sign in
+            </Link>
+          </form>
+        </div>
       </div>
     </main>
   );
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -5,42 +5,44 @@ const signin = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <header>Sign in</header>
-      </div>
-      <div>
-        <p className={`m-0 max-w-[30ch] text-4xl`}>Sign In</p>
-        <form className="flex flex-col space-y-4">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-          />
-          <button type="submit" className="button">
-            Sign in
-          </button>
-          <p className={`m-0 max-w-[30ch] text-sm`}>Don't have an account,</p>
-          <Link
-            href={"/signup"}
-            className="left-0 top-0 flex w-full justify-center  border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-          >
-            Signup
-          </Link>
-          <Link
-            href={"/signin/forgotpassword"}
-            className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-          >
-            forgot password
-          </Link>
-        </form>
+        <div>
+          <header>Sign in</header>
+        </div>
+        <div>
+          <p className={`m-0 max-w-[30ch] text-4xl`}>Sign In</p>
+          <form className="flex flex-col space-y-4">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+            />
+            <button type="submit" className="button">
+              Sign in
+            </button>
+            <p className={`m-0 max-w-[30ch] text-sm`}>Don't have an account,</p>
+            <Link
+              href={"/signup"}
+              className="left-0 top-0 flex w-full justify-center  border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+            >
+              Signup
+            </Link>
+            <Link
+              href={"/signin/forgotpassword"}
+              className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+            >
+              forgot password
+            </Link>
+          </form>
+        </div>
       </div>
     </main>
   );
