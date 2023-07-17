ec182fd0ff69419e92bc029af90f60187ddee1d8
taye000 <taylorgitari@gmail.com>
2023-07-17 15:12:04 +0200

    home

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,25 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div>
-        {/* <Image
-          className="mb-8 align-left"
-          src={imanilogo}
-          alt="imani.js Logo"
-          width={100}
-          height={25}
-          priority
-        /> */}
-        {/* <header>Imani Escrow Services</header> */}
         <section id="showcase">
           <div className="container">
-            <h1>Imani Escrow Services</h1>
+            <h1>
+              Helping small businesses and individuals to transact safely and
+              securely.
+            </h1>
+          </div>
+        </section>
+        <div className="container">
+          <section id="main">
+            <h1>Welcome to imani escrow services</h1>
+          </section>
+          <aside id="sidebar">
             <p>
               Helping small businesses and individuals to transact safely and
               securely.
             </p>
-          </div>
-        </section>
+          </aside>
+        </div>
       </div>
     </main>
   );
