b3acc4edd1c0a3d6831eac11f7379e07f007bfe7
taye000 <taylorgitari@gmail.com>
2023-07-17 13:31:02 +0200

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,80 +1,50 @@
 import Image from "next/image";
 import Link from "next/link";
-import imanilogo from "./public/imanilogo.png";
+import imanilogo from "../public/imanilogo.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <div>
-        <Image
+        {/* <Image
           className="mb-8 align-left"
           src={imanilogo}
           alt="imani.js Logo"
           width={100}
           height={25}
           priority
-        />
-      <div>
+        /> */}
         <header>Imani Escrow Services</header>
-      </div>
-      </div>
-      <div className="z-10 w-full  items-center justify-between font-mono text-sm lg:flex">
-        <Link
-          href={"/"}
-          className=" left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-        >
-          Home
-        </Link>
-        <Link
-          href={"/about"}
-          className=" left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-        >
-          About
-        </Link>
-        <Link
-          href={"/signin"}
-          className=" left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-        >
-          Signin
-        </Link>
-        <Link
-          href={"/signup"}
-          className=" left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-        >
-          Signup
-        </Link>
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Link
-          href={"/faq"}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            FAQ's{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Most frequently asked questions, answered.
-          </p>
-        </Link>
-
-        <Link
-          href={"/contact"}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Contact{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Get in touch with us.
-          </p>
-        </Link>
+        <nav id="navbar">
+          <div className="container">
+            <ul>
+              <li>
+                <Link href={"/"}>Home</Link>
+              </li>
+              <li>
+                <Link href={"/about"}>About</Link>
+              </li>
+              <li>
+                <Link href={"/contact"}>Contact</Link>
+              </li>
+              <li>
+                <Link href={"/faq"}>FAQ's</Link>
+              </li>
+              <li>
+                <Link href={"/signin"}>Signin</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <section id="showcase">
+          <div className="container">
+            <h1>Imani Escrow Services</h1>
+            <p>
+              Helping small businesses and individuals to transact safely and
+              securely.
+            </p>
+          </div>
+        </section>
       </div>
     </main>
   );
