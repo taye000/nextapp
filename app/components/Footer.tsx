67842065f2f9aeb8e9fc2a8ce8e09a9aa91fdb55
taye000 <taylorgitari@gmail.com>
2023-07-26 15:04:04 +0200

    footer refactor

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,20 +1,25 @@
 import Link from "next/link";
 import React from "react";
-import { AiOutlineTwitter, AiOutlineLinkedin, AiOutlineInstagram, AiOutlineFacebook } from "react-icons/ai";
+import {
+  AiOutlineTwitter,
+  AiOutlineLinkedin,
+  AiOutlineInstagram,
+  AiOutlineFacebook,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
     <footer
       id="main-footer"
-      className="mx-auto bg-blue-900 max-w p-4 items-center"
+      className="mx-auto bg-blue-900 max-w p-2 items-center"
     >
-      <div className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0">
-        <div className="flex flex-col-reverse items-venter justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
-        <div className="mx-auto my-6 text-center text-white md:hidden">Copyright &copy; 2023 Imani Escrow Services</div>
-          <div>
-            <img src="/imanilogo.png" className="h-8" alt=""></img>
+      <div className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-col md:space-y-0">
+        <div className="flex flex-col-reverse items-venter justify-between space-y-8 md:flex-col md:space-y-0">
+          <div className="mx-auto my-4 text-center text-white md:hidden">
+            Copyright &copy; 2023 Imani Escrow Services
           </div>
-          <div className="flex justify-center space-x-4">
+          <div className="flex flex-col justify-between md:flex md:space-x-4 md:flex-row">
+          <div className="flex justify-between space-x-4">
             <a
               href="https://twitter.com/taye000"
               rel="noreferrer"
@@ -56,18 +61,23 @@ const Footer = () => {
               />
             </a>
           </div>
-          <div className="flex flex-col justify-between">
+          <div className="flex flex-col justify-between mt-4">
             <form>
               <div className="flex space-x-3">
-            <input
-              type="text"
-              className="flex-1 px-4 rounded-full focus:outline-none"
-              placeholder="Enter your email"
-            ></input>
-            <button className="bg-blue-700 rounded-full text-white p-3 md:p3 md:rounded-full md:bg-blue-700 hover:bg-blue-500">Go</button>
+                <input
+                  type="text"
+                  className="flex-1 px-4 rounded-full focus:outline-none"
+                  placeholder="Enter your email"
+                ></input>
+                <button className="bg-blue-700 rounded-full text-white p-3 md:p3 md:rounded-full md:bg-blue-700 hover:bg-blue-500">
+                  Go
+                </button>
               </div>
             </form>
-          <div className="hidden text-white md:block">Copyright &copy; 2023 Imani Escrow Services</div>
+          </div>
+          </div>
+          <div className="hidden text-white  md:block">
+            Copyright &copy; 2023 Imani Escrow Services
           </div>
         </div>
       </div>
