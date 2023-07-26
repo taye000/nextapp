a7fb82b01ce1afc019c10813bff8fc4ac9f7d747
taye000 <taylorgitari@gmail.com>
2023-07-26 12:58:27 +0200

    footer icons

--- app/components/CallToAction.tsx
+++ app/components/CallToAction.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const CallToAction = () => {
   return (
     <section id="cta" className="bg-blue-500 mt-4">
-      <div className="container flex flex-col items-center justify-between px-6 py-24 mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0">
+      <div className="container flex flex-col items-center justify-between px-6 py-10 mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0">
         <h2 className="text-5xl font-bold leading-tight text-center text-white md:max-w-xl md:text-4xl md:text-left">
           Secure your online purchases with our Escrow services today.
         </h2>
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,11 +1,75 @@
+import Link from "next/link";
 import React from "react";
+import { AiOutlineTwitter, AiOutlineLinkedin, AiOutlineInstagram, AiOutlineFacebook } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <footer id="main-footer" className="mx-auto max-w p-4 items-center">
-      <hr className="w-full h-0.5 mx-auto mt-8 bg-blue-800 border-0"></hr>
-      <div className="flex  flex-row items-venter justify-center">
-        <p>Copyright &copy; 2023 Imani Escrow Services</p>
+    <footer
+      id="main-footer"
+      className="mx-auto bg-blue-900 max-w p-4 items-center"
+    >
+      <div className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0">
+        <div className="flex flex-col-reverse items-venter justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
+        <div className="mx-auto my-6 text-center text-white md:hidden">Copyright &copy; 2023 Imani Escrow Services</div>
+          <div>
+            <img src="/imanilogo.png" className="h-8" alt=""></img>
+          </div>
+          <div className="flex justify-center space-x-4">
+            <a
+              href="https://twitter.com/taye000"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <AiOutlineTwitter
+                className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+                size={30}
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/taylor-gitari-15b290133/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <AiOutlineLinkedin
+                className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+                size={30}
+              />
+            </a>
+            <a
+              href="https://instagram.com/taylor.gitari?igshid=OGQ5ZDc2ODk2ZA=="
+              rel="noreferrer"
+              target="_blank"
+            >
+              <AiOutlineInstagram
+                className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+                size={30}
+              />
+            </a>
+            <a
+              href="https://www.facebook.com/taylor.abbruzzi/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <AiOutlineFacebook
+                className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+                size={30}
+              />
+            </a>
+          </div>
+          <div className="flex flex-col justify-between">
+            <form>
+              <div className="flex space-x-3">
+            <input
+              type="text"
+              className="flex-1 px-4 rounded-full focus:outline-none"
+              placeholder="Enter your email"
+            ></input>
+            <button className="bg-blue-700 rounded-full text-white p-3 md:p3 md:rounded-full md:bg-blue-700 hover:bg-blue-500">Go</button>
+              </div>
+            </form>
+          <div className="hidden text-white md:block">Copyright &copy; 2023 Imani Escrow Services</div>
+          </div>
+        </div>
       </div>
     </footer>
   );
