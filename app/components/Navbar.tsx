5eb6725c26a977dd2f7b3e9710dea0096a15958c
taye000 <taylorgitari@gmail.com>
2023-07-27 10:56:32 +0200

    nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,9 @@
 "use client";
-import Link from "next/link";
 import React, { useState } from "react";
+import Link from "next/link";
+import { useTheme } from "next-themes";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
+import { RiMoonFill, RiSunLine } from "react-icons/ri";
 
 interface NavItem {
   label: string;
@@ -9,12 +11,14 @@ interface NavItem {
 }
 
 const navItems: Array<NavItem> = [
-  { label: "Home", page: "home" },
+  { label: "Home", page: "/" },
   { label: "About", page: "about" },
   { label: "Contact", page: "contact" },
   { label: "FAQ's", page: "faq" },
 ];
 const Navbar = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
   const [navbar, setNavbar] = useState(false);
 
   return (
@@ -30,29 +34,44 @@ const Navbar = () => {
             </button>
           </div>
         </div>
-        <div className="hidden md:flex md:flex-row items-center justify-between py-2">
-          <ul className="md:flex md:flex-row justify-between space-x-4">
-            <li>
-              <Link href={"/"} className="hover:text-gray-500">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href={"/about"} className="hover:text-gray-500">
-                About Us
-              </Link>
-            </li>
-            <li>
-              <Link href={"/contact"} className="hover:text-gray-500">
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link href={"/faq"} className="hover:text-gray-500">
-                FAQ's
-              </Link>
-            </li>
-          </ul>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              {navItems.map((item, index) => {
+                return (
+                  <Link
+                    key={index}
+                    href={item.page}
+                    className={
+                      "block lg:inline-block text-blue-900  hover:text-blue-500 dark:text-black"
+                    }
+                    onClick={() => setNavbar(!navbar)}
+                  >
+                    {item.label}
+                  </Link>
+                );
+              })}
+              {currentTheme === "dark" ? (
+                <button
+                  onClick={() => setTheme("light")}
+                  className="bg-state-100 p2 rounded-xl"
+                >
+                  <RiSunLine size={25} />
+                </button>
+              ) : (
+                <button
+                  onClick={() => setTheme("dark")}
+                  className="bg-state-100 p2 rounded-xl"
+                >
+                  <RiMoonFill size={25} />
+                </button>
+              )}
+            </div>
+          </div>
         </div>
         <Link
           href="#"
