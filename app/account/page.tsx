669195a79cf9ced981418ca20b1d2ad7abb3bbd7
taye000 <taylorgitari@gmail.com>
2023-08-01 18:48:34 +0200

    ytd

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -7,7 +7,7 @@ const account = () => {
         <div className="flex justify-between w-1/2 border rounded-md shadow-md p-6 col-span-1 bg-white lg:col:span-2">
           <div className="flex flex-col w-full pb-4">
             <p className="text-2xl font-bold">$4,000</p>
-            <p className="text-gray-500">Monthly Revenue</p>
+            <p className="text-gray-500">YTD Revenue</p>
           </div>
           <p className="bg-green-200 flex justify-center items-center p-2 rounded-md">
             <span className="text-green-700 text-lg">+10%</span>
