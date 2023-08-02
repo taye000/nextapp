af304b667a2019debd1342707d7a226cd6c5d6b6
taye000 <taylorgitari@gmail.com>
2023-08-02 22:00:43 +0200

    daily sales

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const account = () => {
   return (
     <main className="min-h-screen justify-between mt-5">
-      <div className="grid gap-4 p-4 lg:grid-cols-5">
+      <div className="grid gap-4 p-4 lg:grid-cols-3">
         <div className="flex justify-between w-1/2 border rounded-md shadow-md p-6 col-span-1 bg-white lg:col:span-2">
           <div className="flex flex-col w-full pb-4">
             <p className="text-2xl font-bold">$4,000</p>
@@ -20,6 +20,15 @@ const account = () => {
           </div>
           <p className="bg-green-200 flex justify-center items-center p-2 rounded-md">
             <span className="text-green-700 text-lg">+5%</span>
+          </p>
+        </div>
+        <div className="flex justify-between w-1/2 border rounded-md shadow-md p-6 col-span-1 bg-white lg:col:span-2">
+          <div className="flex flex-col w-full pb-4">
+            <p className="text-2xl font-bold">10</p>
+            <p className="text-gray-500">Daily Sales</p>
+          </div>
+          <p className="bg-green-200 flex justify-center items-center p-2 rounded-md">
+            <span className="text-green-700 text-lg">+2%</span>
           </p>
         </div>
       </div>
