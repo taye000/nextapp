564cd8c44ac4bd837ef7974ff7dfb9786667bd86
taye000 <taylorgitari@gmail.com>
2023-08-08 18:51:32 +0200

    data edit

--- app/data/data.ts
+++ app/data/data.ts
@@ -2,6 +2,7 @@ export const data = [
     {
       id: 1,
       description: 'Iphone X',
+      sellerID: '5f0b4d9b2f3a2c3b6c4d5e6f',
       amount: 2795.95,
       status: 'On Hold',
       mode: 'M-Pesa',
@@ -10,6 +11,7 @@ export const data = [
     {
       id: 2,
       description: 'Mountain Bike',
+      sellerID: '5f0b4d9b2f3a2c3b6b4d5e6k',
       amount: 1195.95,
       status: 'Processing',
       mode: 'PayPal',
@@ -18,6 +20,7 @@ export const data = [
     {
       id: 3,
       description: 'Airbuds pro',
+        sellerID: '5f0b4d9b2f3a2c3b6c4g5e6f',
       amount: 495.85,
       status: 'Completed',
       mode: 'Visa',
