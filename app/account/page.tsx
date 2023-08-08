8cec31ac2f00036449ea9c037a9ebb0a72a4fce6
taye000 <taylorgitari@gmail.com>
2023-08-08 18:16:16 +0200

    data

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { data } from "../data/data";
 
 const account = () => {
   return (
---
+++ app/data/data.ts
@@ -0,0 +1,26 @@
+export const data = [
+    {
+      id: 1,
+      description: 'Iphone X',
+      amount: 2795.95,
+      status: 'On Hold',
+      mode: 'M-Pesa',
+      date: '15 Minutes ago',
+    },
+    {
+      id: 2,
+      description: 'Mountain Bike',
+      amount: 1195.95,
+      status: 'Processing',
+      mode: 'PayPal',
+      date: '23 Minutes ago',
+    },
+    {
+      id: 3,
+      description: 'Airbuds pro',
+      amount: 495.85,
+      status: 'Completed',
+      mode: 'Visa',
+      date: '1 Hour ago',
+    },
+]
