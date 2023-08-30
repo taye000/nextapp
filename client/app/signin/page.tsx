8529fa094e5b1bc1425efdfe01f99dc11e37a629
taye000 <taylorgitari@gmail.com>
2023-08-30 13:04:12 +0200

    refactors

--- client/app/signin/page.tsx
+++ client/app/signin/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { getCookie, storeCookie } from "../utils/tokenUtils";
 
