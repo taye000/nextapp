e2353197288bf6f3fd7ab3eab3582bedf90dde78
taye000 <taylorgitari@gmail.com>
2023-09-21 16:16:13 +0200

    routes chats messages

---
+++ server/src/controllers/chat.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import Chat from "../models/chat";
+
+//controller to get all Chats from db
+export const getUserChats = async (req: Request, res: Response) => {
+  const userId = req.currentUser?.id;
+  try {
+    const chats = await Chat.find({
+      $or: [{ userId }, { clientId: userId }],
+    }); // Retrieve Chats that match either the user ID or the client ID
+    res.status(200).json({ chats });
+  } catch (error: any) {
+    return res.status(404).json({ msg: error.Chat });
+  }
+};
+
+//controller to get a single Chat from db
+export const getUserChat = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const chat = await Chat.findOne({ _id: id });
+    if (!chat) {
+      return res.status(404).json({ msg: "Chat not found" });
+    }
+    res.status(200).json({ chat });
+  } catch (error: any) {
+    return res.status(404).json({ msg: error.Chat });
+  }
+};
--- server/src/controllers/message.ts
+++ server/src/controllers/message.ts
@@ -1,13 +1,29 @@
 import { Request, Response } from "express";
 import Message from "../models/message";
 
-//controller to get all transactions from the blockchain
-export const getMessages = async (_req: Request, res: Response) => {
-    try {
-        const {transactionId} = _req.params;
-      const messages = await Message.find({ transactionId});
-      res.status(200).json({ messages });
-    } catch (error: any) {
-      return res.status(404).json({ msg: error.message });
+//controller to get all messages from db
+export const getUserMessages = async (req: Request, res: Response) => {
+  const userId = req.currentUser?.id;
+  try {
+    const messages = await Message.find({
+      $or: [{ userId }, { clientId: userId }],
+    }); // Retrieve messages that match either the user ID or the client ID
+    res.status(200).json({ messages });
+  } catch (error: any) {
+    return res.status(404).json({ msg: error.message });
+  }
+};
+
+//controller to get a single message from db
+export const getUserMessage = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const message = await Message.findOne({ _id: id });
+    if (!message) {
+      return res.status(404).json({ msg: "Message not found" });
     }
-  };
+    res.status(200).json({ message });
+  } catch (error: any) {
+    return res.status(404).json({ msg: error.message });
+  }
+};
