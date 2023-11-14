f5f3fece52a022f14bef1acd5b8b75328c1fe3b7
taye000 <taylorgitari@gmail.com>
2023-11-14 14:03:01 +0300

    loading

--- client/app/loading.tsx
+++ client/app/loading.tsx
@@ -3,8 +3,18 @@ import React from "react";
 const Loading = () => {
   return (
     <main>
-      <div className="flex justify-center items-center">
-        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-800"></div>
+      <div className="flex-col gap-4 w-full flex items-center justify-center">
+        <div className="w-28 h-28 border-8 text-blue-800 text-4xl animate-spin border-gray-300 flex items-center justify-center border-t-blue-800 rounded-full">
+          <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            height="1em"
+            width="1em"
+            className="animate-ping"
+          >
+            <path d="/imanilogo.png"></path>
+          </svg>
+        </div>
       </div>
     </main>
   );
